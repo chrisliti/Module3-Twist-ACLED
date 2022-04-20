61a6a372014ac214e5708e22857533ec1a3c4893
ChrisMbeva <chrismbev83@gmail.com>
2022-04-20 07:33:45 +0300

    Update app.py
    removed network stuff

--- Module3-Twist-Challenge/app.py
+++ Module3-Twist-Challenge/app.py
@@ -19,7 +19,7 @@ import spacy
 from spacy import displacy
 
 
-nlp = spacy.load('en_core_web_sm')
+#nlp = spacy.load('en_core_web_sm')
 
 from spacy.matcher import Matcher 
 from spacy.tokens import Span 
@@ -153,118 +153,7 @@ with continental_analysis_fatalities:
   st.plotly_chart(Cumulative_fatalities_plot)
 
 
-st.subheader("Country Network Analysis")
-st.write('To run network analysis on a particular country click the button below.')
-if st.button('Run Network Analysis'):
-  with st.spinner("Running Network Analysis"):
-    
-    
-    st.write('Select country of interest for network analysis using knowledge graph')
 
-    selected_country = st.selectbox(
-        'Select Country for Network Analysis',
-        africa['country'].unique())
-    
-    ## Filter for country
-    africa_select = africa[['event_date','year','event_type','actor1','actor2','region','country','notes','fatalities']]
-
-    country_data = africa_select[africa_select['country']==selected_country]
-
-    ## Entities Extraction
-
-    def get_entities(sent):
-
-      ## chunk 1
-      ent1 = ""
-      ent2 = ""
-
-      prv_tok_dep = ""    # dependency tag of previous token in the sentence
-      prv_tok_text = ""   # previous token in the sentence
-
-      prefix = ""
-      modifier = ""
-
-      #############################################################
-      
-      for tok in nlp(sent):
-        ## chunk 2
-        # if token is a punctuation mark then move on to the next token
-        if tok.dep_ != "punct":
-          # check: token is a compound word or not
-          if tok.dep_ == "compound":
-            prefix = tok.text
-            # if the previous word was also a 'compound' then add the current word to it
-            if prv_tok_dep == "compound":
-              prefix = prv_tok_text + " "+ tok.text
-          
-          # check: token is a modifier or not
-          if tok.dep_.endswith("mod") == True:
-            modifier = tok.text
-            # if the previous word was also a 'compound' then add the current word to it
-            if prv_tok_dep == "compound":
-              modifier = prv_tok_text + " "+ tok.text
-          
-          ## chunk 3
-          if tok.dep_.find("subj") == True:
-            ent1 = modifier +" "+ prefix + " "+ tok.text
-            prefix = ""
-            modifier = ""
-            prv_tok_dep = ""
-            prv_tok_text = ""      
-
-          ## chunk 4
-          if tok.dep_.find("obj") == True:
-            ent2 = modifier +" "+ prefix +" "+ tok.text
-            
-          ## chunk 5  
-          # update variables
-          prv_tok_dep = tok.dep_
-          prv_tok_text = tok.text
-      #############################################################
-
-      return [ent1.strip(), ent2.strip()]
-
-  entity_pairs = []
-
-  for i in tqdm(country_data["notes"]):
-    entity_pairs.append(get_entities(i))
-
-  
-  def get_relation(sent):
-    
-    doc = nlp(sent)
-
-    # Matcher class object 
-    matcher = Matcher(nlp.vocab)
-
-    #define the pattern 
-    pattern = [{'DEP':'ROOT'}, 
-              {'DEP':'prep','OP':"?"},
-              {'DEP':'agent','OP':"?"},  
-              {'POS':'ADJ','OP':"?"}] 
-
-    matcher.add("matching_1", None, pattern) 
-
-    matches = matcher(doc)
-    k = len(matches) - 1
-
-    span = doc[matches[k][1]:matches[k][2]] 
-
-    return(span.text)
-
-
-  relations = [get_relation(i) for i in tqdm(country_data["notes"])]
-
-
-  # extract subject
-  source = [i[0] for i in entity_pairs]
-
-  # extract object
-  target = [i[1] for i in entity_pairs]
-
-  kg_df = pd.DataFrame({'source':source, 'target':target, 'edge':relations})
-
-  st.success('Network analysis completed.')
 
 
 
